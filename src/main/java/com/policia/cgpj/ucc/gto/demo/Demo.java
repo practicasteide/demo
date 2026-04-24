f9882671e669ee1b28a513b7f297651662f9c7f7
ucc <ucc@ucc>
2026-04-24 13:17:53 +0200

    Inicio

---
+++ src/main/java/com/policia/cgpj/ucc/gto/demo/Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.policia.cgpj.ucc.gto.demo;
+
+/**
+ *
+ * @author ucc
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
